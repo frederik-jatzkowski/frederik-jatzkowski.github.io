e8798cb58ae0fdd2a3b8dd1d06118ce18cdc4c9c
Frederik Jatzkowski <f.l.jatzkowski@gmail.com>
2024-10-22 09:56:24 +0200

    improved blog pages

--- src/lib/blog/posts/common/BlogPage.svelte
+++ src/lib/blog/posts/common/BlogPage.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
+	import H2 from '$lib/shared/presentation/typography/H2.svelte';
 	import P from '$lib/shared/presentation/typography/P.svelte';
+	import POSTS from '../posts';
 	import type BlogPost from './BlogPost';
+	import BlogPreviewCard from './BlogPreviewCard.svelte';
 
 	export let post: BlogPost;
 </script>
+
+<svelte:head>
+	<title>{post.title}</title>
+	<meta name="description" content={post.intro}>
+</svelte:head>
 
 <article>
 	<h2 class="font-sans text-3xl text-center text-purple-900 mb-4">
 		{post.title}
 	</h2>
-	<h3 class="font-sans text-xl text-center text-purple-900 mb-16">
+	<h3 class="font-sans text-xl text-center text-purple-900 mb-8">
 		{post.subtitle}
 		<div class="h-px bg-amber-400 w-1/2 mx-auto mt-4" />
 	</h3>
-	
+	<time datetime={post.date.toDateString()} class="block my-4">
+		{post.date.toLocaleDateString()}
+	</time>
+
 	{#if post.heroImage}
 		<div class="grid grid-cols-1 mb-8 lg:grid-cols-3 lg:gap-8">
 			<div class="col-span-1">
@@ -31,6 +42,15 @@
 			{post.intro}
 		</P>
 	{/if}
-	
+
 	<svelte:component this={post.body} />
 </article>
+
+<section class="mt-8 p-4 border-2 border-amber-400">
+	<H2>Further Reading</H2>
+	<div class="grid grid-cols-1 gap-4 lg:grid-cols-4">
+		{#each POSTS.filter((each) => each.identifier != post.identifier).slice(0, 4)  as post}
+			<BlogPreviewCard {post} />
+		{/each}
+	</div>
+</section>
