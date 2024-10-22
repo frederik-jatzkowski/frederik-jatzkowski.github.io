f0e8c747536fccb84eead20f012645a9d2bc09c4
Frederik Jatzkowski <f.l.jatzkowski@gmail.com>
2024-10-22 10:22:15 +0200

    fixed graphics bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<header class="px-8 py-2 sticky top-0 left-0 right-0 bg-purple-900 shadow-2xl grid grid-cols-1 lg:px-32 lg:grid-cols-4 md:grid-cols-4">
+<header class="px-8 py-2 sticky top-0 left-0 right-0 bg-purple-900 shadow-2xl grid grid-cols-1 lg:px-32 lg:grid-cols-4 md:grid-cols-4" style="z-index: 100;">
     <a href="/" class="col-span-1">
         <h1 class="font-sans text-center text-white">Frederik Jatzkowski</h1>
         <div class="h-px bg-amber-400 mt-1"/>
